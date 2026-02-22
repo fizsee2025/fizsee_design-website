6d7cebef20c738a7ff62edf7ba8f13c361d234c2
fizsee2025 <fizseedesigns2021@gmail.com>
2026-02-22 17:40:30 +0530

    disabled the telemetry

--- app/components/Contact.tsx
+++ app/components/Contact.tsx
@@ -46,6 +46,7 @@ const Contact = () => {
       icon: <FaMapMarkerAlt />,
       text: "No. 32, Ramanujam Street, Madurantakam, Chengalpattu - 603 306",
     },
+    
     {
       icon: <FaWhatsapp />,
       text: "Chat on WhatsApp",
